e5916a973a08c597eabad20d73a495a00c20fb44
Nkongchou Fomenky <nfomenky@student.ysu.edu>
2020-06-20 21:47:42 -0500

    divided homepage into smaller components, styled menu items

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
 import React from 'react';
 import './App.css';
-import HomePage from './HomePage';
+import HomePage from './pages/homepage/HomePage';
 
 function App() {
   return (
-    <div>
+    <div className='App'>
       <HomePage />
     </div>
   );
