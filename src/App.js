807623c6ebb972d9a735d069bceba1f4e7f09732
Nkongchou Fomenky <nfomenky@student.ysu.edu>
2020-06-30 21:41:39 -0500

    added shop page and collection items with preview

--- src/App.js
+++ src/App.js
@@ -1,13 +1,16 @@
-import React from 'react';
-import './App.css';
-import HomePage from './pages/homepage/HomePage';
+import React from "react";
+import "./App.css";
+import HomePage from "./pages/homepage/HomePage";
+import { Route } from "react-router-dom";
+import ShopPage from "./pages/shop/ShopPage";
 
 function App() {
-  return (
-    <div className='App'>
-      <HomePage />
-    </div>
-  );
+	return (
+		<div className='App'>
+			<Route exact path='/' component={HomePage} />
+      <Route path='/shop' component={ShopPage} />
+		</div>
+	);
 }
 
 export default App;
