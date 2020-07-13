ff200ef84441ba44fa0b61dc9e38d6807bf29dce
Nkongchou Fomenky <nfomenky@student.ysu.edu>
2020-07-12 19:49:25 -0500

    added sign in page and components

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import HomePage from "./pages/homepage/HomePage";
 import { Route, Switch } from "react-router-dom";
 import ShopPage from "./pages/shop/ShopPage";
 import Header from "./components/header/header";
+import SignInAndSignUp from './pages/sign-in-sign-up/sign-in-sign-up';
 
 function App() {
 	return (
@@ -12,6 +13,7 @@ function App() {
 			<Switch>
 				<Route exact path='/' component={HomePage} />
 				<Route path='/shop' component={ShopPage} />
+				<Route path='/signin' component={SignInAndSignUp} />
 			</Switch>
 		</div>
 	);
---
+++ src/pages/sign-in-sign-up/sign-in-sign-up.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+import SignIn from "../../components/sign-in/sign-in";
+import "./sign-in-sign-up";
+
+const SignInAndSignUp = () => (
+	<div className='sign-in-and-sign-up'>
+        <SignIn />
+    </div>
+);
+
+export default SignInAndSignUp;
