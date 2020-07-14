6ab4c27f8b5ce271cc286c48255aa529611ecd4d
Nkongchou Fomenky <nfomenky@student.ysu.edu>
2020-07-14 15:13:11 -0500

    finished sign in with email and password authentication

--- src/pages/sign-in-sign-up/sign-in-sign-up.jsx
+++ src/pages/sign-in-sign-up/sign-in-sign-up.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import SignIn from "../../components/sign-in/sign-in";
 import "./sign-in-sign-up";
-import SignUp from "../../components/sign-up/sign-up";
+import SignUp from "../../components/sign-up/sign-up"; 
 
 const SignInAndSignUp = () => (
 	<div className='sign-in-and-sign-up'>
